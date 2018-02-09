1fee51e984b9e95ea7c2160669fb08ba100698af
unknown <charlygiraldo333@gmail.com>
2018-02-09 10:33:28 -0500

    funciona

--- prueba.js
+++ prueba.js
@@ -1,4 +1,4 @@
-console.log('funcionaaaaooooo  yyym este otro comitienado otros cambios otro 333');
+console.log('funcionaaaaooooo  yyym este otro comitienado otros cambios otro 44444');
 var mivariable = 1;
 mivariable = 8;
 console.log(mivariable);
